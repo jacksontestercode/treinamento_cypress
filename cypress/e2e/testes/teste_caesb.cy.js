9a7b388d3f321cf463f581859f4ba4fdb9b155ea
jacksontestercode <jacksonfla6@gmail.com>
2024-12-21 23:38:11 -0300

    Atualização do arquivo teste_caesb.cy.js

--- cypress/e2e/testes/teste_caesb.cy.js
+++ cypress/e2e/testes/teste_caesb.cy.js
@@ -1,14 +1,22 @@
 /// <reference types = "Cypress" />
 
-describe('Acessar o CAC TAT', function(){
+describe('Testes GCOM', function(){
     this.beforeEach(function(){
-        cy.visit('https://t1.homolog.caesb.df.gov.br/seguranca/app/login')
+        cy.visit('https://t2.homolog.caesb.df.gov.br/seguranca/app/login')
     })
-    it.only('Preencher os campos usuario e senha e clicar no botão entrar', function(){
+    it('Fazer login', function(){
         cy.get('#j_username').type('jacksonmendes')
         cy.get('#j_password').type('Administrador)))')
         cy.get('#btEntrar').click()
+
+        cy.get(':nth-child(4) > .menu-button > .menu-button-title').click()
+        cy.get(':nth-child(3) > .submenuWrapper > [href="#"] > .label').click()
+        cy.contains('span.label', 'Pesquisar Atendimento', { timeout: 10000 }).should('be.visible').click();
+//        cy.get('#formAtendimentoPesquisa\\:j_idt404').click();
+        cy.get('#formAtendimentoPesquisa\\:j_idt411', { timeout: 10000 }).should('be.visible').type('2024121833162272');
+        cy.get('span.ui-button-text.ui-c').contains('Pesquisar').click();
+        cy.get('span.ui-button-icon-left.ui-icon.ui-c.ui-icon-triangle-1-s').click();
+        cy.contains('span.ui-menuitem-text', 'Editar').click();
      })
     
 });
-

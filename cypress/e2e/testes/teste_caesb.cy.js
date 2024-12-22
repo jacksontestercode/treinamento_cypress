857589d5f0725109cc8ee19acbc36470e0b957d4
jacksontestercode <jacksonfla6@gmail.com>
2024-12-21 22:13:40 -0300

    Atualização do arquivo cypress.config.js

--- cypress/e2e/testes/teste_caesb.cy.js
+++ cypress/e2e/testes/teste_caesb.cy.js
@@ -2,17 +2,13 @@
 
 describe('Acessar o CAC TAT', function(){
     this.beforeEach(function(){
-        cy.visit('https://t2.homolog.caesb.df.gov.br/seguranca/app/login')
+        cy.visit('https://t1.homolog.caesb.df.gov.br/seguranca/app/login')
     })
-    it('verifica titulo da aplicação', function(){
-        cy.title().should('ERP Caesb')
-    });
-    it.only('preenche os campos obrigatorios e envia o formulario', function(){
-        cy.get('#firstName').type('Jackson')        
-        cy.get('#lastName').type('Mendes')
-        cy.get('#email').type('jacksonfla6@gmail.com')
-        cy.get('#open-text-area').type('Teste')        
-        cy.get('button[type="submit"]').click()
-    });
+    it.only('Preencher os campos usuario e senha e clicar no botão entrar', function(){
+        cy.get('#j_username').type('jacksonmendes')
+        cy.get('#j_password').type('Administrador)))')
+        cy.get('#btEntrar').click()
+     })
+    
 });
 
